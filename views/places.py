e756cb648fe6af3048ba8ee5e3356d398aa3c12a
HAMarat <smile56680@gmail.com>
2023-02-04 15:34:25 +0300

    feat: add filter by price and city

--- views/places.py
+++ views/places.py
@@ -1,4 +1,5 @@
 from flask_restx import Resource, Namespace
+from flask import request
 
 from dao.model.place import PlaceSchema
 from implemented import place_service
@@ -9,12 +10,22 @@ place_ns = Namespace('places')
 @place_ns.route('/')
 class PlaceView(Resource):
     def get(self):
+        filter_city = request.args.get('city')
+        if filter_city:
+            return PlaceSchema(many=True).dump(place_service.get_by_city(filter_city))
+
+        price_from = request.args.get('from')
+        price_to = request.args.get('to')
+
+        if price_from and price_to:
+            return PlaceSchema(many=True).dump(place_service.get_by_price(price_from, price_to))
+
         places = place_service.get_all()
-        return PlaceSchema(many=True).dumps(places), 200
+        return PlaceSchema(many=True).dump(places), 200
 
 
 @place_ns.route('/<int:pid>')
 class PlacesViews(Resource):
     def get(self, pid):
         place = place_service.get_one(pid)
-        return PlaceSchema().dumps(place)
+        return PlaceSchema().dump(place)
